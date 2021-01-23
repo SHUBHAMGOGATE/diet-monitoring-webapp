cc4473b95a3a411aaacd59f69011a298579e9c68
Yash Gandhi <51473894+yash4gandhi@users.noreply.github.com>
2021-01-23 13:09:45 +0530

    Update OnboardingMessage.js

--- src/Components/OnboardingMessage.js
+++ src/Components/OnboardingMessage.js
@@ -6,7 +6,7 @@ class OnboardingMessage extends Component {
   render() {
     return (
       <div className="OnboardingMessage">
-        <h3>Welcome to EasyCal!</h3>
+        <h3>Welcome to OnTrack!</h3>
         <p>This is your daily log, where you can record the foods you eat throughout the day.  You can navigate between days using the calendar control 
         below.  It'd be a good idea to <Link to={deploymentConfig().baseUrl + "/me"}>set up your goals</Link> first, 
         then come back to this screen by clicking the "Today's Log" button in the navigation bar.</p>
